0fcd6023dbac1fb366223ae100d9b776a7b5d605
hyj1991 <yeekwanvong@gmail.com>
2020-01-11 21:25:16 +0800

    docs: 增加如何单独编译 V8 引擎

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,8 @@ module.exports = {
         sidebarDepth: 3,
         children: [
           '/0x01_编译完整的 LLVM',
-          '/0x02_管理 Ubuntu 下的版本'
+          '/0x02_管理 Ubuntu 下的版本',
+          '/0x03_单独编译 V8 引擎'
         ]
       }
     ]
